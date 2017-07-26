2375bb41ca2ca72b77a107035f730de8a9d03d67
Alex <alexh.90@live.com>
2017-07-25 22:39:36 -0700

    Update main.py
    cleaned up commentary

--- create_box/main.py
+++ create_box/main.py
@@ -13,25 +13,4 @@ def create_box(height, width, character):
 print(create_box(3, 4, '*'))
 print(create_box(2, 2, '@'))
 
-""" 
-For an extra challenge, make it so only the outer border of the box shows and it is not filled in. Make this a separate function and add unit tests for it.
-
-Oh, also-- Remember that when you have a for loop like `for w in range(5)` that `w` will take on the value of each number when your loop is going through, for example:
-for w in range(5):
-    print('W is: {}'.format(w))  # <-- Just formating the string here, plugging in the `w` variable to the {}'s inside the string
-
-'W is: 0'
-'W is: 1'
-'W is: 2'
-'W is: 3'
-'W is: 4'
-
-create_empty_box(5, 5, '*')
-*****
-*   *
-*   *
-*   *
-*****
-"""
-
 
